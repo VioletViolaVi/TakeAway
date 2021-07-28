2ca8d2d98a69b56c450fd3876558587788c644cf
VioletViolaVi <vivian.v18723@gmail.com>
2021-07-28 22:09:57 +0000

    Change images and theme.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // https://css-tricks.com/snippets/jquery/simple-auto-playing-slideshow/
-  $("#slideshow > div:gt(0)").hide();
 
-  setInterval(function () {
-    $("#slideshow > img:first")
-      .fadeOut(1000)
-      .next()
-      .fadeIn(1000)
-      .end()
-      .appendTo("#slideshow");
-  }, 3000);
 });
